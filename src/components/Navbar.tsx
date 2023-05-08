8c2ebcc6de45f0c42ad96860e01f45c3ffbaa4db
javiermrtnez <javiermartinezbueno@gmail.com>
2023-05-08 17:59:04 +0200

    feat: add burguer menu and responsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,10 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import joinClassNames from '../utils/functions/classNames';
 import Logo from './Logo';
 import { useAuthContext } from '../context/AuthContext';
 import LogInButton from './LogInButton';
 import SignOutButton from './SignOutButton';
+import { useState } from 'react';
 
 const NAVIGATION = [
   {
@@ -23,6 +24,19 @@ const NAVIGATION = [
 const Navbar = () => {
   const { pathname } = useLocation();
   const { user } = useAuthContext();
+  const navigate = useNavigate();
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenuOpen = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+    document.body.style.overflow = !isMobileMenuOpen ? 'hidden' : '';
+  };
+
+  const handleNavigate = (to: string) => {
+    toggleMobileMenuOpen();
+    navigate(to);
+  };
 
   return (
     <header className='sticky top-0 flex w-full z-50 h-[var(--navbar-height)] bg-white bg-opacity-50 shadow-sm backdrop-blur'>
@@ -32,7 +46,7 @@ const Navbar = () => {
         </div>
 
         {user && (
-          <ul className='flex gap-2 justify-center'>
+          <ul className='hidden sm:flex sm:gap-2 sm:justify-center'>
             {NAVIGATION.map(({ name, to }) => (
               <li key={name} className='flex content-center items-center'>
                 <Link
@@ -51,8 +65,58 @@ const Navbar = () => {
           </ul>
         )}
 
-        <div className='flex gap-2 flex-1 justify-end'>
+        <div className='hidden sm:flex sm:flex-1 sm:justify-end'>
           {user ? <SignOutButton /> : <LogInButton />}
+        </div>
+
+        <div className='sm:hidden'>
+          <button onClick={toggleMobileMenuOpen} className='transition-transform w-6 h-10'>
+            <div
+              className={joinClassNames(
+                'w-full h-full flex flex-col pointer-events-none justify-center items-center before:block before:rounded before:h-0.5 before:w-5 before:bg-black before:transition-transform after:block after:rounded after:h-0.5 after:w-5 after:bg-black after:transition-transform',
+                isMobileMenuOpen
+                  ? 'before:translate-y-px before:rotate-45 after:translate-y-0 after:-rotate-45'
+                  : 'before:-translate-y-1 before:rotate-0 after:translate-y-1 after:rotate-0'
+              )}
+            />
+          </button>
+
+          {/* MOBILE MENU */}
+          {isMobileMenuOpen && (
+            <div
+              className='fixed z-10 p-[var(--page-padding)] left-0 right-0 bottom-0 top-[var(--navbar-height)] h-[calc(100vh-var(--navbar-height))] w-screen bg-white/80 backdrop-blur-2xl'
+              onClick={toggleMobileMenuOpen}
+            >
+              <div
+                className='w-full flex flex-col gap-6 rounded-3xl border border-gray-100 bg-white p-5 shadow-md'
+                onClick={(e) => e.stopPropagation()}
+              >
+                {user && (
+                  <ul className='flex flex-col gap-2'>
+                    {NAVIGATION.map(({ name, to }) => (
+                      <li key={name} className='flex content-center items-center'>
+                        <button
+                          className={joinClassNames(
+                            'py-2 px-3 text-gray-600 w-full text-sm text-left rounded-full leading-none hover:text-gray-950 hover:bg-gray-100 transition-colors',
+                            pathname === to
+                              ? ' bg-gray-200 hover:bg-gray-200 text-gray-950 font-medium'
+                              : ''
+                          )}
+                          onClick={() => handleNavigate(to)}
+                        >
+                          {name}
+                        </button>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+
+                <div className='[&>button]:w-full'>
+                  {user ? <SignOutButton /> : <LogInButton />}
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </header>
--- src/components/SignOutButton.tsx
+++ src/components/SignOutButton.tsx
@@ -5,7 +5,7 @@ const SignOutButton = () => {
 
   return (
     <button
-      className='flex content-center items-center px-3 border border-black font-medium rounded-md h-8 text-sm bg-white 
+      className='flex content-center justify-center items-center px-3 border border-black font-medium rounded-md h-8 text-sm bg-white 
             text-black transition-colors hover:bg-black hover:text-white focus:focus:ring-gray-300'
       onClick={handleLogOut}
     >
